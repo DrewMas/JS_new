f13e6ec4e6d214171c99df8fd66255b55c446586
DrewMas <andrewmasnyi@gmail.com>
2021-07-21 15:29:06 +0300

    Additional has been done

--- main.js
+++ main.js
@@ -76,14 +76,13 @@
 // ];
 // // Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)
 //
-//
-//     let filterSort = users.filter(value => {
-//             return value.id % 2 === 0
-//         }
-//     ).sort((a, b) => {
+// let sorter = function (arr) {
+//     let filterSort = users.filter(value => value.id %2 === 0).sort((a, b) => {
 //         return a.id - b.id;
 //     })
-// console.log(filterSort);
+//     console.log(filterSort);
+// }
+// sorter(users);
 
 // CLASS WORK
 //
@@ -247,3 +246,113 @@
 //         console.log(`With Prince ${prince.name} should be ${sinderellas[i].name}`);
 //     }
 // }
+
+// ADDITIONAL
+//
+// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+//
+// {
+//     id: 1,
+//         name: 'Leanne Graham',
+//     username: 'Bret',
+//     email: 'Sincere@april.biz',
+//     address: {
+//     street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//         lat: '-37.3159',
+//             lng: '81.1496'
+//     }
+// },
+//     phone: '1-770-736-8031 x56442',
+//         website: 'hildegard.org',
+//     company: {
+//     name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+// }
+// }
+//
+// class Info {
+//     constructor(id, name, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, catchPhrase,bs) {
+//         this.id = id;
+//         this.name = name;
+//         this.username = username;
+//         this.email = email;
+//         this.address = {city: city, suite: suite, street: street, zipcode: zipcode,geo:{lat: lat, lng: lng}};
+//         this.phone = phone;
+//         this.website = website;
+//         this.company = {name: companyName, catchPhrase: catchPhrase, bs: bs};
+//     }
+// }
+//
+// console.log(new Info(1, 'Leanne Graham', 'Bret', 'Sincere@april.biz', 'Kulas Light', 'Apt. 556', 'Gwenborough', 92998 - 3874, -37.3159, 81.1496,
+//     '1-770-736-8031 x56442', 'hildegard.org', 'Romaguera-Crona', 'Multi-layered client-server neural-net', 'harness real-time e-markets'));
+//
+//
+// -  Створити функцію конструктор / клас  який описує об'єкт тегу
+// Поля :
+//     -назва тегу ()
+// - опис його дій
+// - масив з атрибутами (2-3 атрибути максимум)
+// Кожен атрибут описати як окремий який буде містити
+// -назву атрибуту
+// -опис дії атрибуту
+// інформацію брати з htmlbook.ru
+//
+// Таким чином описати теги
+// -a
+// -div
+// -h1
+// -span
+// -input
+// -form
+// -option
+// -select
+// Приклад результуючого об'єкту
+// {
+//     titleOfTag: 'area',
+//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
+//     attrs: [
+//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+//     {/*some props and values*/},
+//     {/*...*/},
+//     {/*...*/},
+// ]
+//
+// }
+// class Tag {
+//     constructor(titleOfTag, action, titleOfAttr1, actionOfAttr1, titleOfAttr2, actionOfAttr2) {
+//         this.titleOfTag = titleOfTag;
+//         this.action = action;
+//         this.attrs = [{
+//             titleOfAttr1: titleOfAttr1,
+//             actionOfAttr1: actionOfAttr1
+//         }, {
+//             titleOfAttr2: titleOfAttr2,
+//             actionOfAttr2: actionOfAttr2
+//         }];
+//     }
+// }
+//
+// console.log(new Tag('a',
+//     'Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок',
+//     'href',
+//     'Задает адрес документа, на который следует перейти',
+//     'accesskey',
+//     'Активация ссылки с помощью комбинации клавиш'));
+//
+// console.log(new Tag('div',
+//     'Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого',
+//     'align',
+//     'Задает выравнивание содержимого тега <div>',
+//     'title',
+//     'Добавляет всплывающую подсказку к содержимому'));
+//
+// console.log(new Tag('h1',
+//     'тег <h1> представляет собой наиболее важный заголовок первого уровня',
+//     'align',
+//     'Определяет выравнивание заголовка'));
