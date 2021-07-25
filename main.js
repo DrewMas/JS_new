641ce62b9c91ebaec66c4664363a27796a64fa11
DrewMas <andrewmasnyi@gmail.com>
2021-07-26 00:03:51 +0300

    Add

--- main.js
+++ main.js
@@ -176,3 +176,214 @@
 // for (const changeTextElement of changeText) {
 //     changeTextElement.innerText = 'June(Feb)-2021';
 // }
+
+// ADDITIONAL
+//
+//- Створити форму з інпутом для введення ім'я, та action="index2.html".
+// При відправці данних з форми, зберігати ім'я в localstorage, та переходити на index2.html.
+// На index2.html виводити записане в localstorage ім'я в div
+//
+// const form = document.forms.infoForm;
+// const name = form.name;
+// const btn = form.submit;
+//
+// btn.onclick = (ev)=>{
+//     console.log(name.value);
+//     localStorage.setItem('name', name.value);
+// }
+//
+//
+// - Імітуємо наповнення інтернет магазину товарами :
+// Створити форму з наступними полями :
+// - назва товару
+// - кількість товару
+// - ціна товару
+// - картинка товару (посилання з інтернету)
+// Зберігати товари в масив в локалсорадж. При збережені товару з форми, action не повинно відбуватись.
+// створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на сторінці всі товари.
+// На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
+// До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
+//
+//
+//
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+// }];
+// - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+//
+// let addressArr = [];
+// for (const user of users) {
+//     addressArr.push(user.address);
+// }
+// console.log(addressArr);
+//
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+//
+// for (const user of users) {
+//     let userDiv = document.createElement('div');
+//     for (const el in user) {
+//         userDiv.innerHTML = `${user.name} - ${user.age} - ${JSON.stringify(user.address)}`;
+//
+//         document.body.appendChild(userDiv)
+//     }
+//
+//
+// }
+// for (const user of users) {
+//     let userDiv = document.createElement('div');
+//
+//     userDiv.innerHTML = `Name - ${user.name}<br> Age - ${user.age}<br> Address - ${JSON.stringify(user.address)}`;
+//     userDiv.style.margin = '10px';
+//     userDiv.style.backgroundColor = '#f0ffcf';
+//     userDiv.style.width = '600px';
+//     userDiv.style.border = '2px solid black';
+//
+//     document.body.appendChild(userDiv)
+// }
+//
+//
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
+//
+// for (const user of users) {
+//     const divka = document.createElement('div');
+//     document.body.appendChild(divka);
+//     for (const item in user) {
+//         const divochka = document.createElement('div');
+//         divochka.innerText = `${item}: ${JSON.stringify(user[item])}`;
+//         divka.appendChild(divochka);
+//     }
+// }
+//
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
+//
+// for (const user of users) {
+//     const wrapper = document.createElement('div');
+//     const userName = document.createElement('div');
+//     const userAge = document.createElement('div');
+//     const userStatus = document.createElement('div');
+//     const userAddress = document.createElement('div');
+//
+//     userName.innerText = `${user.name}`;
+//     userAge.innerText = `${user.age}`;
+//     userStatus.innerText = `${user.status}`;
+//
+//     for (const item in user.address) {
+//         const userAddressItem = document.createElement('div');
+//         userAddressItem.innerText = `${user.address[item]}`;
+//
+//         userAddress.appendChild(userAddressItem);
+//     }
+//     document.body.appendChild(wrapper);
+//     wrapper.appendChild(userName);
+//     wrapper.appendChild(userAge);
+//     wrapper.appendChild(userStatus);
+//     wrapper.appendChild(userAddress);
+// }
+//
+// - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+// створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
+// Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
+//
+const rules = document.getElementsByTagName('h2');
+const content = document.getElementById('content');
+const list = document.createElement('ul');
+
+for (const rule of rules) {
+    let listItem = document.createElement('li');
+    listItem.appendChild(rule);
+    list.appendChild(listItem);
+    content.appendChild(list);
+}
+//
+// -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+// Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+//
+// 			let rules = [
+// 				{
+// 					title: 'Первое правило Бойцовского клуба.',
+// 					body: 'Никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Второе правило Бойцовского клуба.',
+// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Третье правило Бойцовского клуба.',
+// 					body: 'В схватке участвуют только двое.'
+// 				},
+// 				{
+// 					title: 'Четвертое правило Бойцовского клуба.',
+// 					body: 'Не более одного поединка за один раз.'
+// 				},
+// 				{
+// 					title: 'Пятое правило Бойцовского клуба.',
+// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
+// 				},
+// 				{
+// 					title: 'Шестое правило Бойцовского клуба.',
+// 					body: 'Поединок продолжается столько, сколько потребуется.'
+// 				},
+// 				{
+// 					title: 'Седьмое правило Бойцовского клуба.',
+// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+// 				},
+// 				{
+// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
+// 					body: 'Новичок обязан принять бой.'
+// 				},
+//
+// 			];
