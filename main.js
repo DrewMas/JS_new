41023cd848be6d76467c4a663422b1f8c657d292
DrewMas <andrewmasnyi@gmail.com>
2021-07-26 23:14:17 +0300

    Additional without store

--- main.js
+++ main.js
@@ -204,64 +204,65 @@
 // На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
 // До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
 //
+
 //
 //
-let users = [{
-    name: 'vasya',
-    age: 31,
-    status: false,
-    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-}, {
-    name: 'petya',
-    age: 30,
-    status: true,
-    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-}, {
-    name: 'kolya',
-    age: 29,
-    status: true,
-    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-}, {
-    name: 'olya',
-    age: 28,
-    status: false,
-    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-}, {
-    name: 'max',
-    age: 30,
-    status: true,
-    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-}, {
-    name: 'anya',
-    age: 31,
-    status: false,
-    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-}, {
-    name: 'oleg',
-    age: 28,
-    status: false,
-    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-}, {
-    name: 'andrey',
-    age: 29,
-    status: true,
-    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-}, {
-    name: 'masha',
-    age: 30,
-    status: true,
-    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-}, {
-    name: 'olya',
-    age: 31,
-    status: false,
-    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-}, {
-    name: 'max',
-    age: 31,
-    status: true,
-    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-}];
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+// }];
 // - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
 //
 // let addressArr = [];
@@ -310,46 +311,47 @@ let users = [{
 // - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
 //
 // for (const user of users) {
-//     const wrapper = document.createElement('div');
-//     const userName = document.createElement('div');
-//     const userAge = document.createElement('div');
-//     const userStatus = document.createElement('div');
-//     const userAddress = document.createElement('div');
+//     const div = document.createElement('div');
+//     document.body.append(div);
 //
-//     userName.innerText = `${user.name}`;
-//     userAge.innerText = `${user.age}`;
-//     userStatus.innerText = `${user.status}`;
-//
-//     for (const item in user.address) {
-//         const userAddressItem = document.createElement('div');
-//         userAddressItem.innerText = `${user.address[item]}`;
-//
-//         userAddress.appendChild(userAddressItem);
+//     for (const item in user) {
+//         if (item === 'address') {
+//             const divkaAddress = document.createElement('div');
+//             div.append(divkaAddress);
+//             for (const el in user[item]) {
+//                 const divochka = document.createElement('div');
+//                 divkaAddress.append(divochka);
+//                 divochka.innerText = user.address[el];
+//             }
+//         } else  {
+//             const divka = document.createElement('div');
+//             div.append(divka);
+//             divka.innerText = user[item];
+//         }
 //     }
-//     document.body.appendChild(wrapper);
-//     wrapper.appendChild(userName);
-//     wrapper.appendChild(userAge);
-//     wrapper.appendChild(userStatus);
-//     wrapper.appendChild(userAddress);
 // }
-for (const items of users) {
-    const div = document.createElement('div');
-    document.body.append(div);
-
-    for (const item in items) {
-        const divka = document.createElement('div');
-        divka.innerText = items[item];
-        div.append(divka);
-
-        if (item === `address`){
-            for (const element in items) {
-                const divochka = document.createElement('div');
-                divochka.innerText = element;
-                divka.append(divochka);
-            }
-        }
-    }
-}
+//
+// for (const user of users) {
+//     const div = document.createElement('div');
+//     document.body.append(div);
+//
+//     for (const item in user) {
+//         if (item !== 'address'){
+//             const divka = document.createElement('div');
+//             div.append(divka);
+//             divka.innerText = user[item];
+//     } else{
+//             const divkaAddress = document.createElement('div');
+//             div.append(divkaAddress);
+//             for (const el in user[item]) {
+//                 const divochka = document.createElement('div');
+//                 divkaAddress.append(divochka);
+//                 divochka.innerText = user.address[el];
+//             }
+//         }
+//     }
+// }
+//
 //
 // - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
 // створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
@@ -370,38 +372,57 @@ for (const items of users) {
 // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 // Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
 //
-// 			let rules = [
-// 				{
-// 					title: 'Первое правило Бойцовского клуба.',
-// 					body: 'Никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Второе правило Бойцовского клуба.',
-// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Третье правило Бойцовского клуба.',
-// 					body: 'В схватке участвуют только двое.'
-// 				},
-// 				{
-// 					title: 'Четвертое правило Бойцовского клуба.',
-// 					body: 'Не более одного поединка за один раз.'
-// 				},
-// 				{
-// 					title: 'Пятое правило Бойцовского клуба.',
-// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
-// 				},
-// 				{
-// 					title: 'Шестое правило Бойцовского клуба.',
-// 					body: 'Поединок продолжается столько, сколько потребуется.'
-// 				},
-// 				{
-// 					title: 'Седьмое правило Бойцовского клуба.',
-// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-// 				},
-// 				{
-// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
-// 					body: 'Новичок обязан принять бой.'
-// 				},
+//     let rules = [
+//         {
+//             title: 'Первое правило Бойцовского клуба.',
+//             body: 'Никому не рассказывать о Бойцовском клубе.'
+//         },
+//         {
+//             title: 'Второе правило Бойцовского клуба.',
+//             body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//         },
+//         {
+//             title: 'Третье правило Бойцовского клуба.',
+//             body: 'В схватке участвуют только двое.'
+//         },
+//         {
+//             title: 'Четвертое правило Бойцовского клуба.',
+//             body: 'Не более одного поединка за один раз.'
+//         },
+//         {
+//             title: 'Пятое правило Бойцовского клуба.',
+//             body: 'Бойцы сражаются без обуви и голые по пояс.'
+//         },
+//         {
+//             title: 'Шестое правило Бойцовского клуба.',
+//             body: 'Поединок продолжается столько, сколько потребуется.'
+//         },
+//         {
+//             title: 'Седьмое правило Бойцовского клуба.',
+//             body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//         },
+//         {
+//             title: 'Восьмое и последнее правило Бойцовского клуба.',
+//             body: 'Новичок обязан принять бой.'
+//         },
 //
-// 			];
+//     ];
+//
+//
+//
+//     for (const rule of rules) {
+//         const div = document.createElement('div');
+//         document.body.append(div);
+//         for (const item in rule) {
+//             if (item === 'title'){
+//                 console.log(item);
+//                 const title = document.createElement('h2');
+//                 div.append(title);
+//                 title.innerText = rule[item];
+//             } else {
+//                 const body = document.createElement('p');
+//                 div.append(body);
+//                 body.innerText = rule[item];
+//             }
+//         }
+//     }
