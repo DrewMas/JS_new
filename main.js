0045caa3cfd2ad2429908643f9e872da13965cd7
DrewMas <andrewmasnyi@gmail.com>
2021-07-26 22:06:10 +0300

    Add

--- main.js
+++ main.js
@@ -206,62 +206,62 @@
 //
 //
 //
-// let users = [{
-//     name: 'vasya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-// }, {
-//     name: 'petya',
-//     age: 30,
-//     status: true,
-//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-// }, {
-//     name: 'kolya',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-// }, {
-//     name: 'olya',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-// }, {
-//     name: 'max',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-// }, {
-//     name: 'anya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-// }, {
-//     name: 'oleg',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-// }, {
-//     name: 'andrey',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-// }, {
-//     name: 'masha',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-// }, {
-//     name: 'olya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-// }, {
-//     name: 'max',
-//     age: 31,
-//     status: true,
-//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-// }];
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
 // - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
 //
 // let addressArr = [];
@@ -332,21 +332,39 @@
 //     wrapper.appendChild(userStatus);
 //     wrapper.appendChild(userAddress);
 // }
+for (const items of users) {
+    const div = document.createElement('div');
+    document.body.append(div);
+
+    for (const item in items) {
+        const divka = document.createElement('div');
+        divka.innerText = items[item];
+        div.append(divka);
+
+        if (item === `address`){
+            for (const element in items) {
+                const divochka = document.createElement('div');
+                divochka.innerText = element;
+                divka.append(divochka);
+            }
+        }
+    }
+}
 //
 // - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
 // створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
 // Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
 //
-const rules = document.getElementsByTagName('h2');
-const content = document.getElementById('content');
-const list = document.createElement('ul');
-
-for (const rule of rules) {
-    let listItem = document.createElement('li');
-    listItem.appendChild(rule);
-    list.appendChild(listItem);
-    content.appendChild(list);
-}
+// const rules = document.getElementsByTagName('h2');
+// const content = document.getElementById('content');
+// const list = document.createElement('ul');
+//
+// for (const rule of rules) {
+//     let listItem = document.createElement('li');
+//     listItem.appendChild(rule);
+//     list.appendChild(listItem);
+//     content.appendChild(list);
+// }
 //
 // -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
 // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
